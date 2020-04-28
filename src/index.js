9963ce1f30dffc3c87eae1f1d68c6f0cf634353e
mirror-riddle <hejinpy@163.com>
2020-04-28 21:31:32 +0800

    use react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import './index.css';
 
-const addComponent = () => {
-  const basicApp = document.createElement('h2');
-  basicApp.innerText = [process.env.APP_NAME, process.env.NODE_ENV].join(' - ');
-  basicApp.classList.add(['basic-app']);
-  document.body.append(basicApp);
+const App = () => {
+  return (
+    <div className="basic-app">
+      <h2 className="basic-app-title">{process.env.APP_NAME}</h2>
+      <img className="basic-app-favicon" src="favicon.ico" alt="favicon" />
+    </div>
+  );
 };
 
-addComponent();
+ReactDOM.render(<App />, document.querySelector('#root'));
